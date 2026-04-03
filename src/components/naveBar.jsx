d34fbffa8df3cc7b9ba7a2500d978b103ab6496b
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2026-04-03 20:14:16 +0530

    updated

--- src/components/naveBar.jsx
+++ src/components/naveBar.jsx
@@ -1,47 +1,60 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { FaLinkedin, FaMedium, FaInstagram, FaSquareFacebook, FaSquareGithub } from "react-icons/fa6";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../App.css";
-import { FaLinkedin , FaMedium, FaInstagram, FaSquareFacebook, FaSquareGithub } from "react-icons/fa6";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-       <div className="nav-logo">
 
-  <a href="https://www.linkedin.com/in/darshana-weerasooriya-86a2ba215/" target="_blank" rel="noreferrer">
-    <FaLinkedin size={28} title="LinkedIn" />
-  </a>
+        {/* LEFT ICONS */}
+        <div className="nav-logo">
+          <a href="https://www.linkedin.com/in/darshana-weerasooriya-86a2ba215/" target="_blank" rel="noreferrer">
+            <FaLinkedin size={22} />
+          </a>
 
-  <a href="https://github.com/darshanaweerasooriya" target="_blank" rel="noreferrer">
-    <FaSquareGithub size={28} title="GitHub" />
-  </a>
+          <a href="https://github.com/darshanaweerasooriya" target="_blank" rel="noreferrer">
+            <FaSquareGithub size={22} />
+          </a>
 
-  <a href="https://medium.com/@darshanaweerasooriya11" target="_blank" rel="noreferrer">
-    <FaMedium size={28} title="Medium" />
-  </a>
+          <a href="https://medium.com/@darshanaweerasooriya11" target="_blank" rel="noreferrer">
+            <FaMedium size={22} />
+          </a>
 
-  <a href="https://www.instagram.com/v.i.b.h.a__/" target="_blank" rel="noreferrer">
-    <FaInstagram size={28} title="Instagram" />
-  </a>
+          <a href="https://www.instagram.com/v.i.b.h.a__/" target="_blank" rel="noreferrer">
+            <FaInstagram size={22} />
+          </a>
 
-  <a href="https://www.facebook.com/profile.php?id=61588554345126" target="_blank" rel="noreferrer">
-    <FaSquareFacebook size={28} title="Facebook" />
-  </a>
-
-</div>
-
-        <div className="nav-links">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#skills">Skills</a>
-          <a href="#projects">Projects</a>
-          <a href="#article">Articles</a>
-          <a href="#contact">Contact</a>
+          <a href="https://www.facebook.com/profile.php?id=61588554345126" target="_blank" rel="noreferrer">
+            <FaSquareFacebook size={22} />
+          </a>
         </div>
+
+        {/* HAMBURGER */}
+        <div className="hamburger" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        {/* NAV LINKS */}
+        <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <a href="#home" onClick={toggleMenu}>Home</a>
+          <a href="#about" onClick={toggleMenu}>About</a>
+          <a href="#skills" onClick={toggleMenu}>Skills</a>
+          <a href="#projects" onClick={toggleMenu}>Projects</a>
+          <a href="#article" onClick={toggleMenu}>Articles</a>
+          <a href="#contact" onClick={toggleMenu}>Contact</a>
+        </div>
+
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
