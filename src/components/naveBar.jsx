517549e572f4d4969074d3c1ec828e2f9073bc87
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2026-04-03 20:38:32 +0530

    All done

--- src/components/naveBar.jsx
+++ src/components/naveBar.jsx
@@ -1,55 +1,61 @@
 import React, { useState } from "react";
-import { FaLinkedin, FaMedium, FaInstagram, FaSquareFacebook, FaSquareGithub } from "react-icons/fa6";
+import {
+  FaLinkedin,
+  FaMedium,
+  FaInstagram,
+  FaSquareFacebook,
+  FaSquareGithub,
+} from "react-icons/fa6";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../App.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const handleToggle = () => setMenuOpen(!menuOpen);
+
+  const handleLinkClick = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
       <div className="nav-container">
 
-        {/* LEFT ICONS */}
+        {/* LEFT SOCIAL ICONS */}
         <div className="nav-logo">
           <a href="https://www.linkedin.com/in/darshana-weerasooriya-86a2ba215/" target="_blank" rel="noreferrer">
-            <FaLinkedin size={22} />
+            <FaLinkedin />
           </a>
 
           <a href="https://github.com/darshanaweerasooriya" target="_blank" rel="noreferrer">
-            <FaSquareGithub size={22} />
+            <FaSquareGithub />
           </a>
 
           <a href="https://medium.com/@darshanaweerasooriya11" target="_blank" rel="noreferrer">
-            <FaMedium size={22} />
+            <FaMedium />
           </a>
 
           <a href="https://www.instagram.com/v.i.b.h.a__/" target="_blank" rel="noreferrer">
-            <FaInstagram size={22} />
+            <FaInstagram />
           </a>
 
           <a href="https://www.facebook.com/profile.php?id=61588554345126" target="_blank" rel="noreferrer">
-            <FaSquareFacebook size={22} />
+            <FaSquareFacebook />
           </a>
         </div>
 
         {/* HAMBURGER */}
-        <div className="hamburger" onClick={toggleMenu}>
+        <div className="hamburger" onClick={handleToggle}>
           {menuOpen ? <FaTimes /> : <FaBars />}
         </div>
 
         {/* NAV LINKS */}
         <div className={`nav-links ${menuOpen ? "active" : ""}`}>
-          <a href="#home" onClick={toggleMenu}>Home</a>
-          <a href="#about" onClick={toggleMenu}>About</a>
-          <a href="#skills" onClick={toggleMenu}>Skills</a>
-          <a href="#projects" onClick={toggleMenu}>Projects</a>
-          <a href="#article" onClick={toggleMenu}>Articles</a>
-          <a href="#contact" onClick={toggleMenu}>Contact</a>
+          <a href="#home" onClick={handleLinkClick}>Home</a>
+          <a href="#about" onClick={handleLinkClick}>About</a>
+          <a href="#skills" onClick={handleLinkClick}>Skills</a>
+          <a href="#projects" onClick={handleLinkClick}>Projects</a>
+          <a href="#article" onClick={handleLinkClick}>Articles</a>
+          <a href="#contact" onClick={handleLinkClick}>Contact</a>
         </div>
 
       </div>
